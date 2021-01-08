28e6f6013d1416508cc18cd623892c2573523055
Sulav Shrestha <gilm.dav007@gmail.com>
2021-01-08 23:51:02 +0545

    Use configurations from a config.cfg file

--- include/Utils.h
+++ include/Utils.h
@@ -1,7 +1,16 @@
 #pragma once
 
 #include <random>
+#include <iostream>
+#include <fstream>
 
+extern std::string DEBUG_STRING;
+extern bool DEBUG;
+extern int ITERATION;
+extern std::string TEST_STRING;
+
+// Declarations
+int parse_config(std::string cfgFile);
 
 namespace rand_generator {
     double rand_gen_norm_dist();
